34ce317ba05bef3236b72bb50cdfcb6182ad6155
william <william807803@hotmail.com>
2020-03-03 19:47:26 +0800

    as

--- config/config.default.js
+++ config/config.default.js
@@ -33,15 +33,33 @@ module.exports = appInfo => {
 
   config.sequelize = process.env.NODE_ENV=="production"?{
     dialect: 'postgres',
-    connectionUri:process.env.DATABASE_URL
+    connectionUri:process.env.DATABASE_URL,
+    define: {
+      freezeTableName: true,
+      underscored: false,
+    },
   }:{
-    dialect: 'mysql',
-    host: '127.0.0.1',
-    port: 3306,
-    username:'root',
-    password:'fuckfuck',
+    // dialect: 'mysql',
+    // host: '127.0.0.1',
+    // port: 3306,
+    // username:'root',
+    // password:'fuckfuck',
     // database: 'blog_development',
-    database: 'blog',
+
+    dialect: 'postgres',
+    host: '127.0.0.1',
+    port: 5432,
+    username:'postgres',
+    password:'fuckfuck',
+    database: 'postgres',
+
+    // dialect: 'postgres',
+    // host: 'ec2-184-72-236-3.compute-1.amazonaws.com',
+    // port: 5432,
+    // username:'nfqvfsqhqhhyvk',
+    // password:'13d3d79483a2502bdf1520f529e87a348271834796c533eb1895e51980ddc8bc',
+    // database: 'dc5g7vq0t1q0iq',
+
     define: {
       freezeTableName: true,
       underscored: false,
